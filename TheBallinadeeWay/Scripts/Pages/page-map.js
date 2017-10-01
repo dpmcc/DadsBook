27f4dee912832e1261c26ff7ea79e83754615aca
dpmcc <denispaulmccarthy@gmail.com>
2017-10-01 11:55:36 +0100

    Join Greensock club and testing of handwriting

--- TheBallinadeeWay/Scripts/Pages/page-map.js
+++ TheBallinadeeWay/Scripts/Pages/page-map.js
@@ -7,8 +7,9 @@
              var that = this;
              _.each(Alphabet, function(item){
                  
-                    TweenMax.to(that.Get('B').$Element, 1, {rotation:360, x:30,y:200,fill:'#ff0000',scale:2}, 2.5);
-                  TweenMax.to(that.Get('B').$Element, 3, {fill:'#0000ff',scale:2}, 2.5);
+                   // TweenMax.to(that.Get('B').$Element, 1, {rotation:360, x:30,y:200,fill:'#ff0000',scale:2}, 2.5);
+                    TweenMax.to(that.Get('B').$Element, 3, {skewX:90}, 2.5);
+                     TweenMax.to(that.Get('E').$Element, 3, {skewX:90}, 2.5);
                  
              })
              
@@ -16,7 +17,21 @@
          }
      });
        
-        
+       var demo = document.getElementById("demo");
+var tl = new TimelineMax({repeat:-1});
+var ease = Linear.easeNone;
+
+TweenMax.set(demo, {autoAlpha:1});
+TweenMax.set("path", {drawSVG:0, stroke:"#42a6e0"});
+TweenMax.set("ellipse", {autoAlpha:0, fill:"#42a6e0"});
+
+tl.to("#hPipe", 0.35, {drawSVG:true, ease:ease});
+tl.to("#hBody", 0.75, {drawSVG:true, ease:ease});
+tl.to("#mainPath", 5, {drawSVG:true, ease:ease});
+tl.to("#dot2", 0.15, {autoAlpha:1});
+tl.to("#dot1", 0.15, {autoAlpha:1});
+tl.to("#tCross", 0.25, {drawSVG:true, ease:ease});
+tl.to("path, ellipse", 0.75, {autoAlpha:0}, "+=1"); 
         
     });
 })();
